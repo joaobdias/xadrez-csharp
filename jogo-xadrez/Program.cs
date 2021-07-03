e30b6940bb4c54d8820fffd41484f5927787f326
joaobdias <joaobdiasbr@gmail.com>
2021-07-03 13:18:52 -0300

    adicionada classe posicao

--- jogo-xadrez/Program.cs
+++ jogo-xadrez/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace jogo_xadrez
 {
@@ -6,7 +7,7 @@ namespace jogo_xadrez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
