fc6ae689a6cacc422c12864e8ce6d372787e33f0
joaobdias <joaobdiasbr@gmail.com>
2021-07-03 13:09:20 -0300

    inicio projeto

---
+++ jogo-xadrez/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jogo_xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
