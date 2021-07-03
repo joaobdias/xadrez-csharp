5658329289225109c8fc35d2ef02aa4374e2f067
joaobdias <joaobdiasbr@gmail.com>
2021-07-03 15:43:30 -0300

    primeiras peças criadas

--- jogo-xadrez/Program.cs
+++ jogo-xadrez/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace jogo_xadrez
 {
@@ -8,6 +9,10 @@ namespace jogo_xadrez
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(Cor.Preto, tab), new Posicao(0, 0));
+            tab.colocarPeca(new Rei(Cor.Branco, tab), new Posicao(1, 3));
+            tab.colocarPeca(new Torre(Cor.Branco, tab), new Posicao(2,4));
 
             Tela.imprimirTabuleiro(tab);
 
---
+++ jogo-xadrez/xadrez/Torre.cs
@@ -0,0 +1,17 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class Torre : Peca
+    {
+        public Torre(Cor cor, Tabuleiro tab) : base(cor, tab)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "T";
+        }
+    }
+}
