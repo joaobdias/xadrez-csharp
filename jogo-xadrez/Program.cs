bd32eaf16a7ab04bac164632489c2589bdee902e
joaobdias <joaobdiasbr@gmail.com>
2021-07-03 15:55:51 -0300

    primeiros tratamentos de erros

--- jogo-xadrez/Program.cs
+++ jogo-xadrez/Program.cs
@@ -8,14 +8,20 @@ namespace jogo_xadrez
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Torre(Cor.Preto, tab), new Posicao(0, 0));
-            tab.colocarPeca(new Rei(Cor.Branco, tab), new Posicao(1, 3));
-            tab.colocarPeca(new Torre(Cor.Branco, tab), new Posicao(2,4));
+                tab.colocarPeca(new Torre(Cor.Preto, tab), new Posicao(0, 0));
+                tab.colocarPeca(new Rei(Cor.Branco, tab), new Posicao(1, 3));
+                tab.colocarPeca(new Torre(Cor.Branco, tab), new Posicao(2, 4));
 
-            Tela.imprimirTabuleiro(tab);
-
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
