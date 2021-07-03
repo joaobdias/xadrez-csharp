25d5cfde5ebed1cf7ec7b71b249dcb8efad3ed31
joaobdias <joaobdiasbr@gmail.com>
2021-07-03 13:50:51 -0300

    representacao visual do tabuleiro

--- jogo-xadrez/Program.cs
+++ jogo-xadrez/Program.cs
@@ -7,6 +7,9 @@ namespace jogo_xadrez
     {
         static void Main(string[] args)
         {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab);
 
         }
     }
