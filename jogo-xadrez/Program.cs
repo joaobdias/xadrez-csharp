262bc2820cda53ead6b11cc8765c08e8b8074176
joaobdias <joaobdiasbr@gmail.com>
2021-07-07 13:13:52 -0300

    mudanca de cor das pecas

--- jogo-xadrez/Program.cs
+++ jogo-xadrez/Program.cs
@@ -8,11 +8,20 @@ namespace jogo_xadrez
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
-            Console.WriteLine(pos);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine(pos.toPosicao());
+                tab.colocarPeca(new Torre(Cor.Preto, tab), new Posicao(0, 0));
+                tab.colocarPeca(new Rei(Cor.Branco, tab), new Posicao(1, 3));
+                tab.colocarPeca(new Torre(Cor.Branco, tab), new Posicao(2, 4));
 
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
